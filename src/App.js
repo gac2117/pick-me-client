af32aca489c4ac7ffa4620fd2e8e9f25e261daf7
Grace Choi <oiograceoio@gmail.com>
2019-07-16 13:41:38 -0400

    updated basic App info

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I'm so hungry working on this app...What should I eat..?
-        </p>
-        <p>If this app was finished, I suppose it would tell me..haha..</p>
-      </header>
+
     </div>
     );
   }
