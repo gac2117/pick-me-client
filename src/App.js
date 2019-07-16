d4af694f5249942789027dffc0b3e2c7be7b3049
Grace Choi <oiograceoio@gmail.com>
2019-07-16 16:58:22 -0400

    added Restaurant Container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
+import RestaurantContainer from './containers/RestaurantContainer'
 
 class App extends Component {
   render() {
     return (
     <div className="App">
-
+      <h1>Pick Me Pick Me!</h1>
+      <RestaurantContainer />
+      <button>Add a Restaurant</button>
     </div>
     );
   }
