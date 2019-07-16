73ca6ab6734bb390fdffd8346254dadfdf293120
Grace Choi <oiograceoio@gmail.com>
2019-07-16 14:19:55 -0400

    added thunk

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 // import rootReducer from './reducers/index'
-import { thunk } from 'react-thunk;'
+import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
+
+const users = () => []
+const reducer = users
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(manageRestaurant, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
 	<Provider store={store}>
