c1145aaa3cbec4f75d187cd4f1d06b8fc1341b31
Grace Choi <oiograceoio@gmail.com>
2019-07-16 13:32:13 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm so hungry working on this app...What should I eat..?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>If this app was finished, I suppose it would tell me..haha..</p>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
