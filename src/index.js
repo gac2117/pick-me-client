2dee2dfb48109b69153d18cf9ffcd9851c2358cc
Grace Choi <oiograceoio@gmail.com>
2019-07-16 14:31:33 -0400

    added restaurantReducer

--- src/index.js
+++ src/index.js
@@ -4,16 +4,14 @@ import './index.css';
 import App from './App';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-// import rootReducer from './reducers/index'
+import restaurantReducer from './reducers/restaurantReducer'
 import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 
-const users = () => []
-const reducer = users
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(restaurantReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
 	<Provider store={store}>
