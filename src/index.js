fd6d8721ac1640dcd15873c9a20b28032431ae80
Grace Choi <oiograceoio@gmail.com>
2019-07-17 15:20:32 -0400

    added rootReducer and combineReducers

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import './index.css';
 import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
-import restaurantReducer from './reducers/restaurantReducer'
-
-
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-
 
 ReactDOM.render(
 	<Provider store={store}>
