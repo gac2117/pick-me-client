f6f382d8ae7ded21353f12e7415a031ceff44dff
Grace Choi <oiograceoio@gmail.com>
2019-07-19 11:18:26 -0400

    modified NavBar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+  const link = {
+    width: '100px',
+    padding: '12px',
+    margin: '0 6px 6px',
+    background: 'pink',
+    textDecoration: 'none',
+    color: 'white',
+  }
+
   return (
     <div className="navbar">
-       <ul>
-      	<li><NavLink to="/" exact>Home</NavLink></li>
-      	<li><NavLink to="/new" exact>Add a Restaurant</NavLink></li>
-      	<li><NavLink to="/random" exact>Pick Me</NavLink></li>
-       </ul>
+      <NavLink to="/" exact style={link} activeStyle={{background: 'red'}}>Home</NavLink>
+      <NavLink to="/new" exact style={link} activeStyle={{background: 'red'}}>Add a Restaurant</NavLink>
+      <NavLink to="/random" exact style={link} activeStyle={{background: 'red'}}>Pick Me</NavLink>
     </div>
   );
 };
