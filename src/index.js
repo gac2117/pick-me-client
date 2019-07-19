587faacbc53f7591b67b0f92f791f1a09833b953
Grace Choi <oiograceoio@gmail.com>
2019-07-19 10:07:46 -0400

    added react router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import RestaurantInput from './components/restaurants/RestaurantInput'
 import store from './store';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Link,  } from 'react-router-dom';
 
 ReactDOM.render(
 	<Provider store={store}>
-	  <App />
+		<Router>
+			<React.Fragment>
+				<Route path='/' component={App} />
+				<Route exact path='/new' component={RestaurantInput} />
+			</React.Fragment>
+		</Router>
 	</Provider>,
 	document.getElementById('root')
 	);
