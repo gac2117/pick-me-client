a91d871925eb4198dafdd5590769651f249bfe75
Grace Choi <oiograceoio@gmail.com>
2019-07-20 17:04:57 -0400

    fixed error message

--- src/components/tags/TagList.js
+++ src/components/tags/TagList.js
@@ -5,11 +5,13 @@ class TagList extends Component {
 
 	render() {
 		const { tags, restaurantId } = this.props;
+		// eslint-disable-next-line
 		const ownTags = tags.filter(t => t.restaurant_id == restaurantId);
 		const renderTags = ownTags.map(t => {
 			return (
-				<Tag key={t.id}
-				 tag={t} />
+				<div key={t.id}>
+					<Tag tag={t} />
+				 </div>
 			)
 		})
 
