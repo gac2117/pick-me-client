32a7f18e380fa0b376c514f969be89eaf15b8c4d
Grace Choi <oiograceoio@gmail.com>
2019-07-20 14:11:57 -0400

    trying to retrieve restaurant tags

--- src/components/tags/TagList.js
+++ src/components/tags/TagList.js
@@ -2,20 +2,27 @@ import React, { Component } from 'react';
 import Tag from './Tag';
 
 class TagList extends Component {
-	render() {
-		const {tags} = this.props;
 
-		const renderTags = tags.map(t => {
-			return (
+	renderTags = (tags) => {
+		if (!Array.isArray(tags) || !tags.length) {
+			return null;
+		} else {
+			tags.map(t => {
+				return (
 				<>
 				Tags: <Tag key={t.id} tag={t} />
 				</>
-			)
-		})
+				)
+			})
+		}
+	}
+
+	render() {
+		const {tags} = this.props;
 
 		return (
 			<>
-			{renderTags}
+			{this.renderTags(tags)}
 			</>
 		)
 		
--- src/containers/TagContainer.js
+++ src/containers/TagContainer.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react';
 import TagInput from '../components/tags/TagInput';
 import TagList from '../components/tags/TagList';
+import { connect } from 'react-redux';
 
 class TagContainer extends Component {
 
+	state = {
+		restaurant: {}
+	}
+
+	renderTagList = (tags) => {
+		if (!Array.isArray(tags) || !tags.length) {
+			return null;
+		} else {
+			return (
+				<>
+				<TagList tags={this.props.restaurant.attributes.tags} />
+				</>
+			)
+		}
+	}
+
+	findRestaurant = (id) => {
+		this.props.restaurants.map(r => {
+			if (r.id === id) {
+				this.setState({
+					restaurant: r
+				})
+			} else {
+				return null;
+			}
+		})
+	}
+
 	render() {
+		console.log(this.props.restaurant.id)
 		return (
 			<>
-			<TagList tags={this.props.restaurant.attributes.tags} />
-			<TagInput restaurantId={this.props.restaurant.id} />
+			<br />
+			{this.findRestaurant(this.props.restaurant.id)}
+			<h3>Tags:</h3>
+			{this.renderTagList(this.props.restaurant.attributes.tags)}
+			<br />
+			Add Tags: <TagInput restaurantId={this.props.restaurant.id} />
 			</>
 		)
 	}
 }
+
+const mapStateToProps = (state) => ({
+  restaurants: state.restaurants.items
+})
 
 export default TagContainer;
