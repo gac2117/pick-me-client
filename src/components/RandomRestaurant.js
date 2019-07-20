b0142b868011c8ab99efa0f1dec86a9150e99f57
Grace Choi <oiograceoio@gmail.com>
2019-07-20 11:40:16 -0400

    modified random restaurant component

--- src/components/RandomRestaurant.js
+++ src/components/RandomRestaurant.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ChosenRestaurant from './restaurants/ChosenRestaurant'
 
 class RandomRestaurant extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       min: 1,
-      max: this.props.restaurants.length,
-      number: 1
+      max: 10,
+      number: 1,
+      restaurant: {attributes: {name: '', location: '', tags: []}}
     }
   }
 
   componentDidMount() {
    this.setState({ number: this.generateNumber(this.state.min, this.state.max)})
   }
+
+  onClick(e) {
+    this.setState({
+      max: this.props.restaurants.length,
+      number: this.generateNumber(this.state.min, this.state.max),
+      restaurant: this.props.restaurants[this.state.number]
+    })
+  } 
   
   generateNumber = (min, max) => {
     return Math.floor(Math.random()*(max-min+1)+min)
   }
   
   render() {
-    
     return (
-      this.state.number
+      <>
+      <button onClick={(e) => this.onClick(e)}>Choose a restaurant</button>
+      <ChosenRestaurant restaurant={this.state.restaurant} />
+      </>
     )
   }
 }
---
+++ src/components/restaurants/ChosenRestaurant.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class ChosenRestaurant extends Component {
+
+	render() {
+		const {restaurant} = this.props;
+		
+		return (	
+			<>
+			<h1>And the chosen restaurant is...
+			<br /><br />
+			{restaurant.attributes.name} 
+			</h1>
+			</>
+		)
+	}
+}
+
+
+export default ChosenRestaurant;
