c5379b6d91d33c82523717fec4aa6555cb1e4e3b
Grace Choi <oiograceoio@gmail.com>
2019-07-23 17:52:17 -0400

    modified text layout

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -17,9 +17,11 @@ class Restaurant extends Component {
 		return (	
 			<div className="Container">
 				<Card style={{ border: '1px solid #4F6367', borderRadius: '5px'}}>
-					<CardHeader style={{backgroundColor: '#7A9E9F', color: 'white'}} tag="h3">{restaurant.attributes.name}</CardHeader>
+					<CardHeader style={{backgroundColor: '#7A9E9F', color: 'white'}} tag="h3">Restaurant Details:</CardHeader>
 					<CardBody>
+						<CardTitle tag="h4">Name: {restaurant.attributes.name}</CardTitle>
 						<CardTitle tag="h4">Location: {restaurant.attributes.location}</CardTitle>
+
 						<br />
 						<Button style={{backgroundColor: '#FE5F55'}} size="sm" onClick={() => this.handleClick()}>Delete Restaurant</Button>
 						<TagContainer restaurant={restaurant} />
