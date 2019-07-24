768e1e40d928adf0ebb702d44791721db5ec225a
Grace Choi <oiograceoio@gmail.com>
2019-07-24 10:52:42 -0400

    changed to functional component

--- src/components/restaurants/RestaurantList.js
+++ src/components/restaurants/RestaurantList.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
-class RestaurantList extends Component {
-	
-	render() {
+function RestaurantList({restaurants}){
+	console.log({restaurants})
 
-		const restaurantList = this.props.restaurants.map(r => {
-			return (
-				<ListGroupItem 
-				  key={r.id} 
-				  tag={Link} 
-				  to={{
-					pathname: '/restaurant',
-					state: {restaurant: r}
-				}} action>{r.attributes.name}</ListGroupItem>
-			)
-		})
-		
-		return(
-			<div className="Container">
-				<h3>Current List of Restaurants:</h3>
-				<br />
-				<ListGroup style={{ border: '1px solid #4F6367', borderRadius: '5px'}}>
-			   		{restaurantList}  
-		    	</ListGroup>
-			</div>
+	const restaurantList = restaurants.map(r => {
+		return (
+			<ListGroupItem 
+			  key={r.id} 
+			  tag={Link} 
+			  to={{
+				pathname: '/restaurant',
+				state: {restaurant: r}
+			}} action>{r.attributes.name}</ListGroupItem>
 		)
-	}
+	})
+		
+	return(
+		<div className="Container">
+			<h3>Current List of Restaurants:</h3>
+			<br />
+			<ListGroup style={{ border: '1px solid #4F6367', borderRadius: '5px'}}>
+		   		{restaurantList}  
+	    	</ListGroup>
+		</div>
+	)	
 }
 
-const mapStateToProps = state => ({
-	restaurants: state.restaurants.items
-})
+function mapStateToProps(state) {
+	return {restaurants: state.restaurants.items};
+}
 
 export default connect(mapStateToProps)(RestaurantList);
